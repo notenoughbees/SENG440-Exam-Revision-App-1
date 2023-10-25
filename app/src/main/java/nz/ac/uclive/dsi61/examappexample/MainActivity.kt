85f926c750bbfd499d8db24d883d438c7e789ab2
dhsim <dsi61@uclive.ac.nz>
2023-10-25 13:23:11 +1300

    Add 2nd textfield; add multiline textfield; align other textfields to the right.

--- app/src/main/java/nz/ac/uclive/dsi61/examappexample/MainActivity.kt
+++ app/src/main/java/nz/ac/uclive/dsi61/examappexample/MainActivity.kt
@@ -2,21 +2,16 @@ package nz.ac.uclive.dsi61.examappexample
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.media.ToneGenerator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
@@ -24,12 +19,11 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import nz.ac.uclive.dsi61.examappexample.ui.theme.ExamAppExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,27 +46,34 @@ class MainActivity : ComponentActivity() {
                         )
                     }
                 ) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .padding(innerPadding) // put textfield below the topappbar
+                    Box(
+                        modifier = Modifier.fillMaxSize().padding(16.dp) // put padding around entire content: outer
                     ) {
-                        @Composable
-                        fun MyTextField() {
-                            //TODO: make fn to save/retrieve
-                            var selectedFrom by rememberSaveable{mutableStateOf((sharedPreferences.getString("from", "Default Value") ?: "Default Value"))}   //saveable: persist config changes. TODO SHEET: remember mutableStateOf(
+                        Column(
+                            modifier = Modifier
+                                .padding(innerPadding) // put textfield below topappbar rather than behind
+                        ) {
+                            @Composable
+                            fun MyTextField(label: String, preferenceKey: String, modifier: Modifier = Modifier) {
+                                //TODO: make fn to save/retrieve
+                                var selectedValue by rememberSaveable{mutableStateOf((sharedPreferences.getString(preferenceKey, "<default value>") ?: "<default value>"))}   //saveable: persist config changes. TODO SHEET: remember mutableStateOf(
 
-                            TextField(
-                                value = selectedFrom,
-                                onValueChange = { newFrom ->
-                                    selectedFrom = newFrom
-                                    sharedPreferences.edit().putString("from", newFrom).apply()
-                                },
-                                label = { Text(text = "From") }
-                            ) //TODO SHEET: no {} here
+                                TextField(
+                                    value = selectedValue,
+                                    onValueChange = { newValue ->
+                                        selectedValue = newValue
+                                        sharedPreferences.edit().putString(preferenceKey, newValue).apply()
+                                    },
+                                    label = { Text(text = label) },
+                                    modifier = modifier
+                                ) //TODO SHEET: no {} here
+                            }
+
+                            MyTextField("From", "from", Modifier.align(Alignment.End))
+                            MyTextField("To", "to", Modifier.align(Alignment.End))
+                            MyTextField("", "items", Modifier.fillMaxSize()) // multiline textbox gets diff modifier
+
                         }
-                        MyTextField()
-
-
                     }
                 }
             })
